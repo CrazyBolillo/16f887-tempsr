3af3ab022f15f41e92baac51a19756338abbbe7c
CrazyBolillo <40015596+CrazyBolillo@users.noreply.github.com>
2021-10-26 09:51:51 -0500

    Finished program

--- main.c
+++ main.c
@@ -16,6 +16,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define HEATER_PR2 255
+#define HEATER_CONTROL PORTDbits.RD7 = PORTDbits.RD6
+
+#define FAN_PR2 100
+#define FAN_CONTROL PORTDbits.RD5 = PORTDbits.RD4
+
+#define PLAY_SOUND PIE1bits.TMR2IE = 1; T2CONbits.TMR2ON = 1;
+#define STOP_SOUND PIE1bits.TMR2IE = 0; T2CONbits.TMR2ON = 0; PORTDbits.RD7 = 0;
+
+char buzzer = 0;
 char buffer[8];
 const float resolution = 48.87585533;
 uint16_t adc_value = 0;
@@ -42,6 +52,15 @@ void uart_write(char *string) {
     }
 }
 
+// Poor mans PWM
+void __interrupt() play_sound() {
+    if (PIR1bits.TMR2IF == 1) {
+        buzzer = !buzzer;
+        PORTCbits.RC3 = buzzer;
+        PIR1bits.TMR2IF = 0;
+    }
+}
+
 void main(void) {
     OSCCON = 0x79;
     
@@ -60,9 +79,27 @@ void main(void) {
     
     PORTC = 0x00;
     TRISC = 0x80;
+    PORTD = 0x00;
+    TRISD = 0x00;
     PORTE = 0x00;
     TRISE = 0x0C;
     
+    /**
+     * ------------------------------
+     *  Setup to play sound using PWM
+     * ------------------------------
+     */
+    // Prescaler 16 - TMR2 off
+    T2CON = 0x03;
+    
+    // Enable interrupts
+    PIE1bits.TMR2IE = 1;
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
+    
+    
+    read_adc();
+    temperature = 0;
     while (1) {
         read_adc();
         sprintf(buffer, "%d%d.%d%d\r\n", 
@@ -72,6 +109,25 @@ void main(void) {
                 ((uint8_t)(temperature % 10))
                 );
         uart_write(buffer);
-        __delay_ms(1000);
+        
+        if (temperature >= 6000) {
+            FAN_CONTROL = 1;
+            PR2 = FAN_PR2;
+            PLAY_SOUND;
+        }
+        else {
+            FAN_CONTROL = 0;
+        }
+        if (temperature  <= 1500) {
+            HEATER_CONTROL = 1;
+            PR2 = HEATER_PR2;
+            PLAY_SOUND;
+        }
+        else {
+            HEATER_CONTROL = 0;
+        }
+        if ((temperature < 6000) && (temperature > 1500)) {
+            STOP_SOUND;
+        }
     }
 }
